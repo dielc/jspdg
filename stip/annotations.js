9a3421912005a436800bad40cb2b36a186becc32
dielc <diel.caroes@gmail.com>
2015-04-29 16:55:05 +0200

    annotations

--- stip/annotations.js
+++ stip/annotations.js
@@ -5,7 +5,9 @@
 // Client
 var client_annotation = "@client";
 var server_annotation = "@server";
-var assumes_annotation = "@assumes"
+var assumes_annotation = "@assumes";
+var use_handler_annotation    = "@useHandler";
+var define_handler_annotation = "@defineHandlers";
 
 
 // Client annotations is @client in comment
@@ -21,3 +23,11 @@ var isServerAnnotated = function (node) {
 var isAssumesAnnotated = function (string) {
 	return string.indexOf(assumes_annotation) != -1;
 }
+
+var isUseHandlerAnnotatedNode = function (node) {
+	return node.leadingComment && node.leadingComment.value.indexOf(use_handler_annotation) != -1;
+};
+
+var isDefineHandlerAnnotated = function (node) {
+	return node.leadingComment && node.leadingComment.value.indexOf(define_handler_annotation) != -1;
+};
